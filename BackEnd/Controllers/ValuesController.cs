14d44356659eb2dab68de0a8cf0496869bccee45
Ainhoa Goyeneche <Ainhoa Goyeneche>
2017-04-28 15:17:52 -0300

    Proyecrto BackEnd

---
+++ BackEnd/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Services;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
+using BackEnd.Models;
+
+namespace BackEnd.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        // GET api/values
+        [HttpGet]
+        public IHttpActionResult Get(String value)
+		{
+            Respuesta resp = new Respuesta();
+
+			resp.code = "00";
+			resp.description = "OK";
+			resp.data = "UTC ISO DATE";
+
+			return Ok(resp);
+		}
+		
+        // POST api/values
+        [HttpPost]
+        public HttpResponseMessage Post(Recibe aData)
+        {
+            Respuesta resp = new Respuesta();
+
+            Regex r = new Regex("^[A-Za-z ]+$");
+            if (r.IsMatch(aData.word) == true)
+            {
+                resp.code = "00";
+                resp.description = "OK";
+                resp.data = aData.word.ToUpper();
+                return Request.CreateResponse(HttpStatusCode.OK, resp);
+            }
+            else
+            {
+                resp.code = "01";
+                resp.description = "NO_OK";
+                resp.data = String.Empty;
+                return Request.CreateResponse(HttpStatusCode.BadRequest, resp);
+            }
+        }
+    }
+
+    public class Recibe
+    {
+        public String word { get; set; }
+    }
+}
---
+++ BackEnd/Models/Respuesta.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEnd.Models
+{
+    public class Respuesta
+    {
+        public String code { get; set; }
+        public String description { get; set; }
+        public String data { get;set; }
+    }
+}
